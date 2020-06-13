26a97b143ffa9409c3198748d5dbb9eadb1fb7be
bbrown1867 <bbrown1867@gmail.com>
2020-06-13 19:21:32 -0400

    Add UART printf

--- src/hal/stm32f7xx_hal_msp.c
+++ src/hal/stm32f7xx_hal_msp.c
@@ -1,6 +1,6 @@
 /**
   ******************************************************************************
-  * @file    Templates/Src/stm32f7xx_hal_msp.c
+  * @file    stm32f7xx_hal_msp.c
   * @author  MCD Application Team
   * @brief   HAL MSP module.
   ******************************************************************************
@@ -17,73 +17,71 @@
   ******************************************************************************
   */
 
-/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
 
-/** @addtogroup STM32F7xx_HAL_Examples
-  * @{
-  */
+/* UART configuration for USART3, which is connected to the ST-Link and allows */
+#define USARTx                           USART3
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART3_CLK_ENABLE();
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOD_CLK_ENABLE()
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART3_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART3_RELEASE_RESET()
+#define USARTx_TX_PIN                    GPIO_PIN_8
+#define USARTx_TX_GPIO_PORT              GPIOD
+#define USARTx_TX_AF                     GPIO_AF7_USART3
+#define USARTx_RX_PIN                    GPIO_PIN_9
+#define USARTx_RX_GPIO_PORT              GPIOD
+#define USARTx_RX_AF                     GPIO_AF7_USART3
 
-/** @addtogroup Templates
-  * @{
-  */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-
-/** @defgroup HAL_MSP_Private_Functions
-  * @{
-  */
-
-/**
-  * @brief  Initializes the Global MSP.
-  * @param  None
-  * @retval None
-  */
 void HAL_MspInit(void)
 {
+
 }
 
-/**
-  * @brief  DeInitializes the Global MSP.
-  * @param  None
-  * @retval None
-  */
 void HAL_MspDeInit(void)
 {
+
 }
 
-/**
-  * @brief  Initializes the PPP MSP.
-  * @param  None
-  * @retval None
-  */
-/*void HAL_PPP_MspInit(void)
-{*/
-/*}*/
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+    RCC_PeriphCLKInitTypeDef RCC_PeriphClkInit;
+    GPIO_InitTypeDef GPIO_InitStruct;
 
-/**
-  * @brief  DeInitializes the PPP MSP.
-  * @param  None
-  * @retval None
-  */
-/*void HAL_PPP_MspDeInit(void)
-{*/
-/*}*/
+    /* Enable GPIO TX/RX clock */
+    USARTx_TX_GPIO_CLK_ENABLE();
+    USARTx_RX_GPIO_CLK_ENABLE();
 
-/**
-  * @}
-  */
+    /* Select SysClk as source of USARTx clocks */
+    RCC_PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+    RCC_PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_SYSCLK;
+    HAL_RCCEx_PeriphCLKConfig(&RCC_PeriphClkInit);
 
-/**
-  * @}
-  */
+    /* Enable USARTx clock */
+    USARTx_CLK_ENABLE();
 
-/**
-  * @}
-  */
+    /* UART TX GPIO pin configuration  */
+    GPIO_InitStruct.Pin = USARTx_TX_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = USARTx_TX_AF;
+    HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStruct);
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+    /* UART RX GPIO pin configuration  */
+    GPIO_InitStruct.Pin = USARTx_RX_PIN;
+    GPIO_InitStruct.Alternate = USARTx_RX_AF;
+    HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStruct);
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef *huart)
+{
+    USARTx_FORCE_RESET();
+    USARTx_RELEASE_RESET();
+
+    /* Configure UART TX as alternate function */
+    HAL_GPIO_DeInit(USARTx_TX_GPIO_PORT, USARTx_TX_PIN);
+
+    /* Configure UART RX as alternate function */
+    HAL_GPIO_DeInit(USARTx_RX_GPIO_PORT, USARTx_RX_PIN);
+}
